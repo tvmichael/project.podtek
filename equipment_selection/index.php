bca6d336252c00ee5442792e82dda95267514120
tvmichael <tvmichael@meta.ua>
2019-05-30 23:20:03 +0300

    pool

--- equipment_selection/index.php
+++ equipment_selection/index.php
@@ -1,61 +1,105 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 CJSCore::Init(array("jquery"));
-$APPLICATION->SetAdditionalCss($APPLICATION->GetCurPage()."style.css");
-$APPLICATION->AddHeadScript($APPLICATION->GetCurPage().'script.js');
+$APPLICATION->SetAdditionalCss($APPLICATION->GetCurPage() . "style.css");
+$APPLICATION->AddHeadScript($APPLICATION->GetCurPage() . 'script.js');
 $APPLICATION->SetTitle("Подбор оборудования");
 ?>
-<br>
-<script>var acUrlAjax = '<?=$APPLICATION->GetCurPage().'volume_ajax.php';?>';</script>
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <h4>Подбор оборудования для басейна</h4>
-        <div class="ac-panel-default">
-            <div class="ac-volume-poll-number">0</div>
-            м<span>3</span>
-        </div>
-    </div>
-    <div class="panel-body" id="select-equipment">
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label class="form-group" for="select-length">Длинна:</label>
-                    <input class="form-control" type="text" maxlength="20" id="select-length" placeholder="0" value="">
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label class="form-group" for="select-width">Ширина:</label>
-                    <input class="form-control" type="text" maxlength="20" id="select-width" placeholder="0" value="">
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label class="form-group" for="select-depth">Глубина:</label>
-                    <input class="form-control" type="text" maxlength="20" id="select-depth" placeholder="0" value="">
-                </div>
-            </div>
-            <div class="col-sm-12">
-                <small>Введите длинну, ширину и глубину басейна в метрах или объем бассейна</small> <br />
-            </div>
-			<div class="col-sm-4">
-                <div class="form-group">
-                    <label class="form-group" for="select-volume">Объем бассейна:</label>
-                    <input class="form-control" type="text" maxlength="20"  id="select-volume" placeholder="0" value="">
-                </div>
-            </div>
-            <div class="col-sm-8">
-                <br>
-                <div class="text-right">
-                    <button class="btn sv-view-n" type="button"> Подобрать оборудование </button>
-                </div>
+    <br>
+    <script>var acUrlAjax = '<?=$APPLICATION->GetCurPage() . 'volume_ajax.php';?>';</script>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="col-xs-12 col-md-9">Подбор оборудования для басейна</h3>
+            <div class="ac-panel-default col-md-3 hidden-xs hidden-sm">
+                <div class="ac-volume-poll-number">0</div>
+                м<span>3</span>
             </div>
         </div>
+        <div class="panel-body" id="select-equipment">
+            <div class="row">
+
+                <div class="col-xs-12 col-md-3 color-question title-question">
+                    Форма бассейна:
+                </div>
+                <label class="col-xs-12 col-md-3 rb-q">
+                    <input class="radio-dot" type="radio" selected="" checked="" name="form_radio_new_field_0006" data-value="ahc-priamokutna">
+                    <span class="ans">Прямоугольная</span>
+                </label>
+                <label class="col-xs-12 col-md-3 rb-q">
+                    <input class="radio-dot" type="radio" name="form_radio_new_field_0006" data-value="ahc-okrugla">
+                    <span class="ans">Округлая</span>
+                </label>
+
+            </div>
+            <div class="row">
+                <div class="col-xs-12 razm-bas color-question title-question">
+                    Размер бассейна:*
+                </div>
+                <div id="ahc-priamokutna">
+                    <div class="col-xs-12 col-md-4">
+                        <label class="form-group" for="select-length">
+                            <input class="form-control inputtext" type="number" maxlength="20" min="0" id="select-length"
+                                   placeholder="длинна" value="">
+                        </label>
+                    </div>
+                    <div class="col-xs-12 col-md-4">
+                        <label class="form-group" for="select-width">
+                            <input class="form-control inputtext" type="number" maxlength="20" min="0" id="select-width"
+                                   placeholder="ширина" value="">
+                        </label>
+                    </div>
+                    <div class="col-xs-12 col-md-4">
+                        <label class="form-group" for="select-depth">
+                            <input class="form-control inputtext" type="number" maxlength="20" min="0" id="select-depth"
+                                   placeholder="глубина" value="">
+                        </label>
+                    </div>
+                </div>
+                <div id="ahc-okrugla">
+                    <div class="col-xs-12 col-md-4">
+                        <label class="form-group" for="select-depth">
+                            <input class="form-control inputtext" type="number" maxlength="20" min="0" id="select-diameter"
+                                   placeholder="диаметр" value="">
+                        </label>
+                    </div>
+                    <div class="col-xs-12 col-md-4">
+                        <label class="form-group" for="select-depth">
+                            <input class="form-control inputtext" type="number" maxlength="20" min="0" id="select-d-depth"
+                                   placeholder="глубина" value="">
+                        </label>
+                    </div>
+                </div>
+                <div class="col-md-12"></div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label class="form-group" for="select-volume"></label>
+                        <input class="form-control inputtext" type="number" maxlength="20" min="0" id="select-volume"
+                               placeholder="Объем бассейна" value="">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-md-9 opis">Введите длинну, ширину и глубину басейна в метрах
+                </div>
+                <div class="col-xs-12 hidden-md hidden-lg hidden-xl">
+
+                    <div class="ac-panel-default">Объем бассейна:
+                        <div class="ac-volume-poll-number">0</div>
+                        м<span>3</span>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-3">
+
+                    <!--div class="text-right"-->
+                    <button class="btn sv-view-n" type="button"> Подобрать</button>
+                    <!--/div-->
+                </div>
+            </div>
+        </div>
     </div>
-</div>
 
-<div class="row">
-    <div class="col-xs-12" id="select-result"></div>
-</div>
+    <div class="row">
+        <div class="col-xs-12" id="select-result"></div>
+    </div>
 
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
--- equipment_selection/script.js
+++ equipment_selection/script.js
@@ -1,23 +1,38 @@
 $(document).ready(function () {
+    var currentPoolType = 'ahc-priamokutna';
+
     function acVolumePoll(){
         var length = parseFloat($("#select-length").val()),
             width = parseFloat($("#select-width").val()),
             depth = parseFloat($("#select-depth").val()),
-            volume = parseFloat($("#select-volume").val()),
+            pVolume = parseFloat($("#select-volume").val()),
+            diameter = parseFloat($("#select-diameter").val()),
+            ddepth = parseFloat($("#select-d-depth").val()),
             volumeMultiple;
 
-        if(length && width && depth){
+        console.log(pVolume + ' >>> ' + $("#select-volume").val());
+
+        if(currentPoolType == 'ahc-priamokutna' && length && width && depth)
+        {
             volumeMultiple = (length * width * depth).toFixed(2);
             $('.ac-panel-default').show();
             $('.ac-volume-poll-number').html(volumeMultiple);
             $('#select-volume').val(volumeMultiple);
             return volumeMultiple;
         }
-        else if (volume){
-            volume = volume.toFixed(2);
+        else if (currentPoolType == 'ahc-okrugla' && diameter && ddepth)
+        {
+            volumeMultiple = (2 * diameter * Math.PI * ddepth).toFixed(2);
             $('.ac-panel-default').show();
-            $('.ac-volume-poll-number').html(volume);
-            return volume;
+            $('.ac-volume-poll-number').html(volumeMultiple);
+            $('#select-volume').val(volumeMultiple);
+            return volumeMultiple;
+        }
+        else if (pVolume){
+            pVolume = pVolume.toFixed(2);
+            $('.ac-panel-default').show();
+            $('.ac-volume-poll-number').html(pVolume);
+            return pVolume;
         }
         else {
             $('.ac-panel-default').hide();
@@ -42,7 +57,29 @@ $(document).ready(function () {
         else $t.css('border-color','red');
         acVolumePoll();
     });
-    $("#select-volume").focusout(function(){acVolumePoll();});
+
+    $("#select-diameter").focusout(function () {
+        var $t = $(this);
+        if (parseFloat($t.val())) $t.css('border-color','#CCD5DB');
+        else $t.css('border-color','red');
+        acVolumePoll();
+    });
+    $("#select-d-depth").focusout(function () {
+        var $t = $(this);
+        if (parseFloat($t.val())) $t.css('border-color','#CCD5DB');
+        else $t.css('border-color','red');
+        acVolumePoll();
+    });
+
+    $("#select-volume").focusout(function(){
+        console.log('#select-volume-volume');
+        $("#select-length").val('');
+        $("#select-width").val('');
+        $("#select-depth").val('');
+        $("#select-diameter").val('');
+        $("#select-d-depth").val('');
+        acVolumePoll();
+    });
 
     $('#select-result').on('click', 'button[data-id]', function () {
         var id = $(this).attr('data-id');
@@ -59,5 +96,24 @@ $(document).ready(function () {
         if(volume && acUrlAjax)
             $.post( acUrlAjax, {volume:volume, action:"equipment"})
                 .done(function( data ) { $('#select-result').html(data);});
+    });
+
+    $("#select-equipment input[type='radio']").click(function (e) {
+        var block = e.target.getAttribute('data-value');
+
+        $('.ac-volume-poll-number').html('0');
+
+        if(block == 'ahc-priamokutna')
+        {
+            currentPoolType = 'ahc-priamokutna';
+            $('#ahc-priamokutna').css('display', 'block');
+            $('#ahc-okrugla').css('display', 'none');
+        }
+        else
+        {
+            currentPoolType = 'ahc-okrugla';
+            $('#ahc-priamokutna').css('display', 'none');
+            $('#ahc-okrugla').css('display', 'block');
+        }
     });
 });
