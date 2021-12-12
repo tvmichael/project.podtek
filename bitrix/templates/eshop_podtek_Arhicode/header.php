3e4297e5cf70c592903d529ecaf6b82f7dd3abbc
tvmichael <michael.tmv@outlook.com>
2021-12-12 21:20:37 +0200

    header

--- bitrix/templates/eshop_podtek_Arhicode/header.php
+++ bitrix/templates/eshop_podtek_Arhicode/header.php
@@ -6,6 +6,27 @@ $theme = COption::GetOptionString("main", "wizard_eshop_bootstrap_theme_id", "bl
 ?>
 <!DOCTYPE html>
 <html xml:lang="<?= LANGUAGE_ID ?>" lang="<?= LANGUAGE_ID ?>">
+<?
+/***** додаємо канонікал для пагінації  ***/
+
+	if (substr_count($_SERVER['REQUEST_URI'], "PAGEN_1=")>0) {
+		$pageCanonikal = '<link rel="canonical" href="';
+		if ($_SERVER["HTTP_X_FORWARDED_PROTO"]='https'){	$pageCanonikal .= $_SERVER["HTTP_X_FORWARDED_PROTO"];
+		}else{
+			$pageCanonikal .= "http";
+		}
+		
+		$pageCanonikal .= '://';
+		$pageCanonikal .= $_SERVER["SERVER_NAME"];
+		$pageCanonikal .= $APPLICATION->GetCurPage(false);
+		$pageCanonikal .= '" />';
+		echo $pageCanonikal;
+		
+	}
+	//echo '<pre> $arCurSection </br>'; print_r($_SERVER); echo '</pre>';
+
+/***** додаємо канонікал для пагінації  ***/
+?>
 <head>
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -39,36 +60,43 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     <title><? $APPLICATION->ShowTitle() ?></title>
 </head>
 <body class="bx-background-image bx-theme-<?= $theme ?>" <?= $APPLICATION->ShowProperty("backgroundImage") ?>>
-<!-- Google Tag Manager (noscript) -->
+<!-- Google Tag Manager (noscript)-->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W6TKM2Z"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 <div id="panel"><? $APPLICATION->ShowPanel(); ?></div>
+<div style="display: none;"><img alt="logo" src="/include/logo.png" srcset="/include/logo.png"></div>
+<?if($USER->IsAdmin()) : ?>
+		<?$admTemplate = "_adm"?>
+<?endif?>
 <div class="bx-wrapper" id="bx_eshop_wrap">
     <header class="bx-header">
         <div class="row bx-header-TopRow">
             <div class="container bx-header-1st-row">
                 <div class="col-lg-8 col-md-8 hidden-sm hidden-xs">
-                    <? $APPLICATION->IncludeComponent(
-                        "bitrix:menu",
-                        "horizontal_multilevel_top_menu",
-                        array(
-                            "ROOT_MENU_TYPE" => "top",
-                            "MENU_CACHE_TYPE" => "A",
-                            "MENU_CACHE_TIME" => "36000000",
-                            "MENU_CACHE_USE_GROUPS" => "Y",
-                            "MENU_THEME" => "site",
-                            "CACHE_SELECTED_ITEMS" => "N",
-                            "MENU_CACHE_GET_VARS" => array(),
-                            "MAX_LEVEL" => "3",
-                            "CHILD_MENU_TYPE" => "left",
-                            "USE_EXT" => "Y",
-                            "DELAY" => "N",
-                            "ALLOW_MULTI_SELECT" => "N",
-                            "COMPONENT_TEMPLATE" => "horizontal_multilevel_top_menu"
-                        ),
-                        false
-                    ); ?>
+				<? $APPLICATION->IncludeComponent(
+					"bitrix:menu", 
+					"horizontal_multilevel_top_menu", 
+					array(
+						"ROOT_MENU_TYPE" => "top".$admTemplate,
+						"MENU_CACHE_TYPE" => "A",
+						"MENU_CACHE_TIME" => "36000000",
+						"MENU_CACHE_USE_GROUPS" => "Y",
+						"MENU_THEME" => "site",
+						"CACHE_SELECTED_ITEMS" => "N",
+						"MENU_CACHE_GET_VARS" => array(
+						),
+						"MAX_LEVEL" => "1",
+						"CHILD_MENU_TYPE" => "",
+						"USE_EXT" => "N",
+						"DELAY" => "N",
+						"ALLOW_MULTI_SELECT" => "N",
+						"COMPONENT_TEMPLATE" => "horizontal_multilevel_top_menu",
+						"COMPOSITE_FRAME_MODE" => "A",
+						"COMPOSITE_FRAME_TYPE" => "AUTO"
+					),
+					false
+				); ?>
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
                     <div class="bx-inc-orginfo">
@@ -78,7 +106,6 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             </div>
         </div>
         <div class="bx-header-section container">
-		
             <div class="row">
                 <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
                     <div class="bx-logo">
@@ -90,86 +117,87 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                         </a>
                     </div>
                 </div>
-                <div class="col-lg-5 col-md-5 col-sm-4 col-xs-12">
+                <div class="col-lg-6 col-md-6 col-sm-5 col-xs-12">
                     <? $APPLICATION->IncludeComponent(
-	"bitrix:search.title", 
-	"visual_old", 
+						"bitrix:search.title", 
+						"visual_old", 
+						array(
+							"NUM_CATEGORIES" => "1",
+							"TOP_COUNT" => "5",
+							"CHECK_DATES" => "N",
+							"SHOW_OTHERS" => "N",
+							"PAGE" => SITE_DIR."search/",
+							"CATEGORY_0_TITLE" => GetMessage("SEARCH_GOODS"),
+							"CATEGORY_0" => array(
+								0 => "iblock_1c_catalog",
+							),
+							"CATEGORY_0_iblock_catalog" => array(
+								0 => "all",
+							),
+							"CATEGORY_OTHERS_TITLE" => GetMessage("SEARCH_OTHER"),
+							"SHOW_INPUT" => "Y",
+							"INPUT_ID" => "title-search-input",
+							"CONTAINER_ID" => "search",
+							"PRICE_CODE" => array(
+								0 => "Интернет-магазин",
+							),
+							"SHOW_PREVIEW" => "Y",
+							"PREVIEW_WIDTH" => "75",
+							"PREVIEW_HEIGHT" => "75",
+							"CONVERT_CURRENCY" => "Y",
+							"COMPONENT_TEMPLATE" => "visual_old",
+							"ORDER" => "date",
+							"USE_LANGUAGE_GUESS" => "Y",
+							"PRICE_VAT_INCLUDE" => "Y",
+							"PREVIEW_TRUNCATE_LEN" => "",
+							"CURRENCY_ID" => "RUB",
+							"CATEGORY_0_iblock_1c_catalog" => array(
+								0 => "7",
+							),
+							"COMPOSITE_FRAME_MODE" => "A",
+							"COMPOSITE_FRAME_TYPE" => "AUTO"
+						),
+						false
+					); ?>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 hidden-xs">
+					<? $APPLICATION->IncludeComponent(
+	"bitrix:sale.basket.basket.line", 
+	"arhi.basket.line_adm", 
 	array(
-		"NUM_CATEGORIES" => "1",
-		"TOP_COUNT" => "5",
-		"CHECK_DATES" => "N",
-		"SHOW_OTHERS" => "N",
-		"PAGE" => SITE_DIR."search/",
-		"CATEGORY_0_TITLE" => GetMessage("SEARCH_GOODS"),
-		"CATEGORY_0" => array(
-			0 => "iblock_1c_catalog",
-		),
-		"CATEGORY_0_iblock_catalog" => array(
-			0 => "all",
-		),
-		"CATEGORY_OTHERS_TITLE" => GetMessage("SEARCH_OTHER"),
-		"SHOW_INPUT" => "Y",
-		"INPUT_ID" => "title-search-input",
-		"CONTAINER_ID" => "search",
-		"PRICE_CODE" => array(
-			0 => "Интернет-магазин",
-		),
-		"SHOW_PREVIEW" => "Y",
-		"PREVIEW_WIDTH" => "75",
-		"PREVIEW_HEIGHT" => "75",
-		"CONVERT_CURRENCY" => "Y",
-		"COMPONENT_TEMPLATE" => "visual_old",
-		"ORDER" => "date",
-		"USE_LANGUAGE_GUESS" => "Y",
-		"PRICE_VAT_INCLUDE" => "Y",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"CURRENCY_ID" => "RUB",
-		"CATEGORY_0_iblock_1c_catalog" => array(
-			0 => "7",
-		),
+		"PATH_TO_BASKET" => SITE_DIR."personal/cart/",
+		"PATH_TO_PERSONAL" => SITE_DIR."personal/order/",
+		"SHOW_PERSONAL_LINK" => "Y",
+		"SHOW_NUM_PRODUCTS" => "Y",
+		"SHOW_TOTAL_PRICE" => "N",
+		"SHOW_PRODUCTS" => "N",
+		"POSITION_FIXED" => "N",
+		"SHOW_AUTHOR" => "Y",
+		"PATH_TO_REGISTER" => SITE_DIR."auth/?register=yes",
+		"PATH_TO_PROFILE" => SITE_DIR."personal/order/",
+		"COMPONENT_TEMPLATE" => "arhi.basket.line_adm",
+		"PATH_TO_ORDER" => SITE_DIR."personal/order/make/",
+		"SHOW_EMPTY_VALUES" => "N",
+		"PATH_TO_AUTHORIZE" => SITE_DIR."auth/",
+		"SHOW_REGISTRATION" => "Y",
+		"SHOW_DELAY" => "Y",
+		"SHOW_NOTAVAIL" => "Y",
+		"SHOW_IMAGE" => "Y",
+		"SHOW_PRICE" => "Y",
+		"SHOW_SUMMARY" => "Y",
+		"HIDE_ON_BASKET_PAGES" => "N",
+		"SHOW_CALL" => "Y",
+		"TYPE_BASKET" => "top",
+		"IBLOCK_TYPE_COMPARE" => "",
+		"IBLOCK_ID_COMPARE" => "",
+		"COMPARE_NAME" => "CATALOG_COMPARE_LIST",
 		"COMPOSITE_FRAME_MODE" => "A",
-		"COMPOSITE_FRAME_TYPE" => "AUTO"
+		"COMPOSITE_FRAME_TYPE" => "AUTO",
+		"MAX_IMAGE_SIZE" => "70"
 	),
 	false
 ); ?>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 hidden-xs">
-                    <? $APPLICATION->IncludeComponent(
-                        "bitrix:sale.basket.basket.line",
-                        "arhi.basket.line",
-                        array(
-                            "PATH_TO_BASKET" => SITE_DIR . "personal/cart/",
-                            "PATH_TO_PERSONAL" => SITE_DIR . "personal/",
-                            "SHOW_PERSONAL_LINK" => "Y",
-                            "SHOW_NUM_PRODUCTS" => "Y",
-                            "SHOW_TOTAL_PRICE" => "N",
-                            "SHOW_PRODUCTS" => "N",
-                            "POSITION_FIXED" => "N",
-                            "SHOW_AUTHOR" => "Y",
-                            "PATH_TO_REGISTER" => SITE_DIR . "login/",
-                            "PATH_TO_PROFILE" => SITE_DIR . "personal/",
-                            "COMPONENT_TEMPLATE" => "arhi.basket.line",
-                            "PATH_TO_ORDER" => SITE_DIR . "personal/order/make/",
-                            "SHOW_EMPTY_VALUES" => "N",
-                            "PATH_TO_AUTHORIZE" => SITE_DIR . "personal/",
-                            "SHOW_REGISTRATION" => "Y",
-                            "SHOW_DELAY" => "Y",
-                            "SHOW_NOTAVAIL" => "Y",
-                            "SHOW_IMAGE" => "Y",
-                            "SHOW_PRICE" => "Y",
-                            "SHOW_SUMMARY" => "Y",
-                            "HIDE_ON_BASKET_PAGES" => "N",
-                            "SHOW_CALL" => "Y",
-                            "TYPE_BASKET" => "top",
-                            "IBLOCK_TYPE_COMPARE" => "",
-                            "IBLOCK_ID_COMPARE" => "",
-                            "COMPARE_NAME" => "CATALOG_COMPARE_LIST",
-                            "COMPOSITE_FRAME_MODE" => "A",
-                            "COMPOSITE_FRAME_TYPE" => "AUTO"
-                        ),
-                        false
-                    ); ?>
-                </div>
+				</div>
             </div>
             <div class="row bx-header-BottomRow">
                 <div class="col-md-3 hidden-xs">
@@ -220,33 +248,27 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                 </div>
 
                 <div class="col-md-9 hidden-xs">
-                    <? $APPLICATION->IncludeComponent(
-                        "bitrix:menu",
-                        "arhi_catalog_horizontal",
-                        array(
-                            "ROOT_MENU_TYPE" => "top_site",
-                            "MENU_CACHE_TYPE" => "A",
-                            "MENU_CACHE_TIME" => "360000",
-                            "MENU_CACHE_USE_GROUPS" => "Y",
-                            "MENU_THEME" => "site",
-                            "MENU_CACHE_GET_VARS" => array(),
-                            "MAX_LEVEL" => "1",
-                            "CHILD_MENU_TYPE" => "left",
-                            "USE_EXT" => "Y",
-                            "DELAY" => "N",
-                            "COMPONENT_TEMPLATE" => "arhi_catalog_horizontal",
-                            "ALLOW_MULTI_SELECT" => "N",
-
-                            "CACHE_SELECTED_ITEMS" => "N",
-                            "COMPOSITE_FRAME_MODE" => "A",
-                            "COMPOSITE_FRAME_TYPE" => "AUTO"
-                        ),
-                        false
-                    );?>
+					<? $APPLICATION->IncludeComponent("bitrix:menu", "horizontal_multilevel_top_menu_right", Array(
+	"ROOT_MENU_TYPE" => "top_site_adm",//.$admTemplate,	// Тип меню для первого уровня
+		"MENU_CACHE_TYPE" => "A",	// Тип кеширования
+		"MENU_CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+		"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+		"MENU_THEME" => "site",
+		"CACHE_SELECTED_ITEMS" => "N",
+		"MENU_CACHE_GET_VARS" => "",	// Значимые переменные запроса
+		"MAX_LEVEL" => "3",	// Уровень вложенности меню
+		"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
+		"USE_EXT" => "Y",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+		"DELAY" => "N",	// Откладывать выполнение шаблона меню
+		"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+		"COMPONENT_TEMPLATE" => "horizontal_multilevel_top_menu1",
+		"COMPOSITE_FRAME_MODE" => "A",	// Голосование шаблона компонента по умолчанию
+		"COMPOSITE_FRAME_TYPE" => "AUTO",	// Содержимое компонента
+	),
+	false
+); ?>
                 </div>
             </div>
-
-
             <? if ($curPage != SITE_DIR . "index.php"): ?>
                 <div class="row">
                     <div class="col-lg-12" id="navigation">
